0809fa77b8244d6801823801bca4c26b68a37651
NRocket <ninamrocket@gmail.com>
2020-06-01 06:54:17 -0700

    initialize

---
+++ Develop/index.js
@@ -0,0 +1,60 @@
+
+
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "What is the description of your project?"
+      },
+      {
+        type: "input",
+        name: "contents",
+        message: "Please input the table of contents for your project."
+      },
+      {
+        type: "input",
+        name: "installation",
+        message: "What installations are needed for your project?"
+      },
+      {
+        type: "input",
+        name: "usage",
+        message: "What is your project used for?"
+      },
+      {
+        type: "input",
+        name: "license",
+        message: "Please provide any licenses."
+      },
+      {
+        type: "input",
+        name: "contributin",
+        message: "Who contributed to this project?"
+      },
+      {
+        type: "input",
+        name: "tests",
+        message: "Provide an example of how to run tests for your project."
+      },
+      {
+        type: "input",
+        name: "questions",
+        message: "Questions?????"
+      },
+
+];
+
+
+function writeToFile(fileName, data) {
+}
+
+function init() {
+
+}
+
+init();
