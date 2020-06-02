f00b20132f910ff42be81d9ea1191edaa9f3b1b2
NRocket <ninamrocket@gmail.com>
2020-06-01 18:36:18 -0700

    asyn function init

--- Develop/index.js
+++ Develop/index.js
@@ -1,3 +1,9 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+const writeFileAsync = util.promisify(fs.writeFile);
 
 
 const questions = [
@@ -5,56 +11,67 @@ const questions = [
         type: "input",
         name: "title",
         message: "What is the title of your project?"
-      },
-      {
+    },
+    {
         type: "input",
         name: "description",
         message: "What is the description of your project?"
-      },
-      {
+    },
+    {
         type: "input",
         name: "contents",
         message: "Please input the table of contents for your project."
-      },
-      {
+    },
+    {
         type: "input",
         name: "installation",
         message: "What installations are needed for your project?"
-      },
-      {
+    },
+    {
         type: "input",
         name: "usage",
         message: "What is your project used for?"
-      },
-      {
+    },
+    {
         type: "input",
         name: "license",
         message: "Please provide any licenses."
-      },
-      {
+    },
+    {
         type: "input",
         name: "contributin",
         message: "Who contributed to this project?"
-      },
-      {
+    },
+    {
         type: "input",
         name: "tests",
         message: "Provide an example of how to run tests for your project."
-      },
-      {
-        type: "input",
-        name: "questions",
-        message: "Questions?????"
-      },
+    },
 
 ];
 
+function promptUser() {
+    return inquirer.prompt(questions);
+}
 
 function writeToFile(fileName, data) {
 }
 
-function init() {
 
-}
+async function init() {
+
+    try {
+        const answers = await promptUser();
+        const markdown = generateMarkdown(answers);
+
+        await writeFileAsync("README.md", markdown);
+
+        console.log("Successfully wrote to README.md");
+
+    } catch (err) {
+        console.log(err);
+    }
+
+};
 
 init();
